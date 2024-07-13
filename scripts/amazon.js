a44c2b0a1fec8cc4cb927f61d2e81e67afb7bd99
FELONYANGO <felixonyango951@gmail.com>
2024-07-13 09:20:28 +0300

    the initial contribution

---
+++ scripts/amazon.js
@@ -0,0 +1,89 @@
+// lets create the array of products in the page
+
+
+    // loop theoufh the products array and create the html for each product
+    let productHtml = "";
+product.forEach(function(product){
+    productHtml += 
+         `
+    <div class="product-container">
+          <div class="product-image-container">
+            <img class="product-image"
+              src="${product.image}">
+          </div>
+
+          <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+          </div>
+
+          <div class="product-rating-container">
+            <img class="product-rating-stars"
+              src="images/ratings/rating-${product.rating.stars *10}.png">
+            <div class="product-rating-count link-primary">
+              ${product.rating.count}
+            </div>
+          </div>
+
+          <div class="product-price">
+           $ ${(product.priceCents / 100).toFixed(2)}
+          </div>
+
+          <div class="product-quantity-container">
+            <select>
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="product-spacer"></div>
+
+          <div class="added-to-cart">
+            <img src="images/icons/checkmark.png">
+            Added
+          </div>
+
+           <button class="js-add-to-cart-button add-to-cart-button button-primary" data-product-id="${product.id}">
+        Add to Cart
+      </button>
+        </div>
+    `;
+    // document.body.innerHTML += productHtml;
+    // console.log(productHtml);
+   
+}
+);
+document.querySelector(".js-products").innerHTML = productHtml;
+
+// make the button add to cart  the product name from the data-product-name attribute
+document.querySelectorAll(".js-add-to-cart-button").forEach((button) => {
+    button.addEventListener("click", () => {
+      const productId = button.getAttribute("data-product-id");
+        console.log(productId);
+
+        // find  if the product is in the cart thendouble the quantity elsr add it to the cart
+
+        const productInCart = cart.find((product) => product.id === productId);
+        if(productInCart){
+          productInCart.quantity += 1;  
+        }
+        else{
+          cart.push({
+            id: productId,
+            quantity: 1
+          });
+        }
+        console.log(cart);
+     
+    });
+  });
+  
+ 
+ 
